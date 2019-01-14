d9c441627e68136281e03b6f4d4661acd8799045
iloveooz <iloveooz@yandex.ru>
2019-01-14 16:36:17 +0700

    добавил Visitor и производныеЭ

--- 4.5/4.5.10.cxx
+++ 4.5/4.5.10.cxx
@@ -1,10 +1,22 @@
 	#include <iostream>
 
+	struct Number;
+	struct BinaryOperation;
+
+	struct Visitor {
+		virtual void visitNumber(Number const * number) = 0;
+		virtual void visitBinaryOperation(BinaryOperation const * binary) = 0;
+		virtual ~Visitor() {}
+	};
+	
 	// структура 
 	struct Expression {
 		
 		// виртуальный метод
 		virtual double evaluate() const = 0;
+
+		// виртуальный метод visit
+		virtual void visit(Visitor * visitor) const = 0;
 		
 		// виртуальный конструктор
 		virtual ~Expression() {}
@@ -17,6 +29,15 @@
 		
 		// виртуальный метод
 		virtual double evaluate () const {
+			return value;
+		}
+
+		// виртуальный метод 
+		void visit(Visitor * visitor) const {
+			visitor->visitNumber(this);
+		}
+		
+		double get_value() const {
 			return value;
 		}
 		
@@ -37,6 +58,23 @@
 		{ }
 
 		// double evaluate () const {}
+		
+		// виртуальный метод
+		void visit(Visitor * visitor) const {
+			visitor->visitBinaryOperation(this);
+		}
+
+		Expression const * get_left() const {
+			return left;
+		}
+
+		Expression const * get_right() const {
+			return right;
+		}
+		
+		char get_op() const {
+			return op;
+		}
 
 		// виртуальный деструктор
 		virtual ~BinaryOperation() {
@@ -62,6 +100,17 @@
 		char op;
 	};
 
+	struct PrintBinaryOperationsVisitor : public Visitor {
+		void visitNumber(Number const * number) { }
+		
+		void visitBinaryOperation(BinaryOperation const * bop) {
+			bop->get_left()->visit(this);
+			std::cout << bop->get_op() << ' ';
+			bop->get_right()->visit(this);
+		}
+	};
+
+
 	int main() {
 		// сначала создаём объекты для подвыражения 4.5 * 5
 		Expression * sube = new BinaryOperation(new Number(4.5), '*', new Number(5));
@@ -74,5 +123,7 @@
 		// тут освобождаются *все* выделенные объекты
 		// (например, sube будет правым операндом expr, поэтому его удалять не нужно)
 		delete expr;
+		
+
 		return 0;
 	}
