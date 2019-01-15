21c74ebd7398cd764754ec49e696b17faf6c4b32
iloveooz <iloveooz@yandex.ru>
2019-01-15 15:14:10 +0700

    done step 4

---
+++ 5.1/step4.cxx
@@ -0,0 +1,52 @@
+	#include <iostream>
+	
+	class Vector {
+	public:
+		double x;
+		double y;
+		
+		Vector(double xi, double yi) : x(xi), y(yi) {}
+		
+		Vector() {
+			x = 0.0;
+			y = 0.0;
+		}
+	};
+	
+	// унарный минус
+	Vector operator - (Vector const & v) {
+		return Vector(-v.x, -v.y);
+	}
+	
+	// бинарный плюс
+	Vector operator + (Vector const &v, Vector const &w) {
+		return Vector(v.x + w.x, v.y + w.y);
+	}
+	
+	// умножение векторa на число
+	Vector operator * (Vector const &v, double d) {
+		return Vector(v.x *d, v.y * d);
+	}
+	
+	// умножение вектора на число
+	Vector operator * (double d, Vector const &v) {
+		return Vector(v * d); // используем перегруженный оператор выше
+	}
+	
+	int main () {
+		
+		Vector A(5.0, 5.0);
+		Vector B;
+		
+		B = -A;
+		
+		B = B * 4;
+		
+		Vector C = B + A;
+		
+		std::cout << "Vector A: A.x = " << A.x << ", A.y = " << A.y << '\n';
+		std::cout << "Vector B: B.x = " << B.x << ", B.y = " << B.y << '\n';
+		std::cout << "Vector C: C.x = " << C.x << ", C.y = " << C.y << '\n';
+		
+		return 0;
+	}
